5ad79c4f44188955f7a621e50eee4ad40c281954
ichiyan <gabriela.balisacan01@gmail.com>
2022-11-10 05:33:11 +0800

    shape

--- shape_recognition.py
+++ shape_recognition.py
@@ -1,5 +1,7 @@
 from PIL import Image
 import numpy as np
+from matplotlib import pyplot as plt
+
 from gaussian_blurv2 import gaussian_blurv2 
 from edge_detectionv2 import canny_edge_detection
 from contour_detectionv2 import find_contours, draw_contours, draw_contour
@@ -19,40 +21,59 @@ def process_img(img):
 
    peri = perimeter(contours[7])
 
-   test = rdp(contours[7], peri * 0.03)
-   print(test)
-   print([tuple(y/50 for y in x) for x in test])
-
-   return img
+   points = rdp(contours[7], peri * 0.03)
+   
+   return img, points
 
 
+def shape(points):
+   num_points = len(points) - 1
+   shapes = {
+      3: 'triangle',
+      4: 'rectangle',
+      5: 'pentagon',
+      6: 'hexagon',
+      7: 'heptagon',
+      8: 'octagon',
+   }
 
-
-img = Image.open('images/shapes5.png')
-processed = process_img(img)
-img.show()
-
-exit()
-
-img = Image.open('images/square.png')
-processed = process_img(img)
-
-img = Image.open('images/pentagon.png')
-processed = process_img(img)
-
-img = Image.open('images/hexagon.png')
-processed = process_img(img)
-
-img = Image.open('images/septagon.png')
-processed = process_img(img)
-
-img = Image.open('images/octagon.png')
-processed = process_img(img)
-
-img = Image.open('images/12.png')
-processed = process_img(img)
+   return shapes.get(num_points, "{num_points}-sided polygon")
 
 
 
+def main():
+
+   images = ['triangle', 'square', 'hexagon', 'octagon']
+
+   fig = plt.figure(figsize=(20, 20))
+   fig.tight_layout()
+   rows = 2
+   columns = 2 
+
+   for ndx, image in enumerate(images):
+      img = Image.open('images/'+ image + '.png')
+      processed_img, points = process_img(img) 
+      shape_name = shape(points)
+
+      # print(points)
+      # print([tuple(y/50 for y in x) for x in points])
+
+      fig.add_subplot(rows, columns, ndx + 1)
+      plt.title(shape_name)
+      plt.imshow(img)
+      # plt.axis('off')
+
+   plt.show()
 
 
+main()
+
+
+# img = Image.open('images/'+ 'circle' + '.png')
+# processed_img, points = process_img(img) 
+# shape_name = shape(points)
+
+# print(points)
+# print([tuple(y/50 for y in x) for x in points])
+
+# img.show()
